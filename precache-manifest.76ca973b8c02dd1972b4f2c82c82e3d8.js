8349b839b5397a4a764525480c9a32e15581f1bb
Bartłomiej Mazur <ab.mazur@gmail.com>
2019-06-15 15:11:13 +0200

    Updates

---
+++ precache-manifest.76ca973b8c02dd1972b4f2c82c82e3d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/spa-01/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/spa-01/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/spa-01/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "1d5caa0974122f2ac12d",
+    "url": "/spa-01/static/js/runtime~main.1d5caa09.js"
+  },
+  {
+    "revision": "72f0bcd31c89c2f2c30f",
+    "url": "/spa-01/static/js/main.72f0bcd3.chunk.js"
+  },
+  {
+    "revision": "15225de600f2dbc17ae6",
+    "url": "/spa-01/static/js/1.15225de6.chunk.js"
+  },
+  {
+    "revision": "72f0bcd31c89c2f2c30f",
+    "url": "/spa-01/static/css/main.5748992a.chunk.css"
+  },
+  {
+    "revision": "65420de85afa4a16e66d6b95fbe063a5",
+    "url": "/spa-01/index.html"
+  }
+];
